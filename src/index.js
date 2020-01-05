bb2c519927ebcef0a938de7e60eb46c0fe686f79
Ilya Kuchin <rorz.ivcyb@gmail.com>
2020-01-05 19:00:35 +0300

    Dev (#6)
    * Install initial packages (#1)
    
    * Add express and index.js file
    
    * Install jest
    
    * Add start script
    
    * Add GET Podcasts API request (#2)
    
    * Add GET Podcast request (#3)
    
    * Add info about episode audio into GET podcast request (#4)
    
    * Add travis config (#5)
    
    * Add mock test

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import { getPodcasts } from "./getPodcasts.js";
+import { getPodcast } from "./getPodcast.js";
+
+const PORT = 5000;
+
+const app = express();
+
+app.use(cors());
+
+app.listen(PORT, () => console.log(`podcast-api is running on port ${PORT}`));
+
+app.get("/podcasts", (req, res) => {
+  getPodcasts(req.query.name).then(podcasts => res.send(podcasts));
+});
+
+app.get("/podcast", (req, res) => {
+  getPodcast(req.query.rss).then(podcast => res.send(podcast));
+});
