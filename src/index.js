ad353409cae3382af618bdc6ec113e26b8a329be
Ilya Kuchin <rorz.ivcyb@gmail.com>
2020-01-19 16:13:48 +0300

    Fix login checks (#14)

--- src/index.js
+++ src/index.js
@@ -6,7 +6,10 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import User from "./models/user.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
+const jwtKey =
+  "WU3Kknc9RAngPjwMhNJhwpCAF1U6EEr9AvllKLqQI3ZeuZmMWgoZ4vdAriODwmo";
 const PORT = 5000;
 mongoose.connect("mongodb://localhost:27017/podcast-player");
 let db = mongoose.connection;
@@ -21,12 +24,26 @@ app.use(bodyParser.json());
 
 app.listen(PORT, () => console.log(`podcast-api is running on port ${PORT}`));
 
-app.get("/podcasts", (req, res) => {
-  getPodcasts(req.query.name).then(podcasts => res.send(podcasts));
+app.get("/podcasts", verifyToken, (req, res) => {
+  jwt.verify(req.token, jwtKey, (err, authData) => {
+    if (err) {
+      console.log(err);
+      res.sendStatus(403);
+    } else {
+      getPodcasts(req.query.name).then(podcasts => res.send(podcasts));
+    }
+  });
 });
 
-app.get("/podcast", (req, res) => {
-  getPodcast(req.query.rss).then(podcast => res.send(podcast));
+app.get("/podcast", verifyToken, (req, res) => {
+  jwt.verify(req.token, jwtKey, (err, authData) => {
+    if (err) {
+      console.log(err);
+      res.sendStatus(403);
+    } else {
+      getPodcast(req.query.rss).then(podcast => res.send(podcast));
+    }
+  });
 });
 
 app.post("/register", (req, res) => {
@@ -49,14 +66,39 @@ app.post("/register", (req, res) => {
   );
 });
 
+function verifyToken(req, res, next) {
+  const bearerHeader = req.headers["authorization"];
+
+  if (typeof bearerHeader !== undefined) {
+    console.log(bearerHeader);
+    const bearer = bearerHeader.split(" ");
+    const bearerToken = bearer[1];
+    req.token = bearerToken;
+    next();
+  } else {
+    res.sendStatus(401);
+  }
+}
+
 app.post("/login", (req, res) => {
-  User.findOne({ username: req.body.username }, (err, obj) => {
-    bcrypt.compare(req.body.password, obj.password, (err, r) => {
-      if (r === true) {
-        res.sendStatus(200);
+  if (req.body.username && req.body.password) {
+    User.findOne({ username: req.body.username }, (err, obj) => {
+      if (obj) {
+        bcrypt.compare(req.body.password, obj.password, (err, r) => {
+          if (r === true) {
+            jwt.sign({ username: req.body.username }, jwtKey, (err, token) => {
+              res.json({ token });
+            });
+          } else {
+            console.log(err);
+            res.sendStatus(401);
+          }
+        });
       } else {
         res.sendStatus(401);
       }
     });
-  });
+  } else {
+    res.sendStatus(401);
+  }
 });
