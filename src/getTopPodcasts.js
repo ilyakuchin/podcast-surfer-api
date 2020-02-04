0af4f48853d552d2f65dffdaa7092e473ba0b159
ilyakuchin <19621723+ilyakuchin@users.noreply.github.com>
2020-02-04 20:56:29 +0300

    Create getTopPodcasts call from itunes api

---
+++ src/getTopPodcasts.js
@@ -0,0 +1,22 @@
+const axios = require('axios');
+
+const URL =
+  'https://rss.itunes.apple.com/api/v1/us/podcasts/top-podcasts/all/10/explicit.json';
+
+function getTopPodcasts() {
+  return axios.get(URL).then(function(response) {
+    const res = response.data.feed.results;
+    let podcastsInfo = [];
+    for (let i = 0; i < res.length; i++) {
+      podcastsInfo.push({
+        id: res[i].collectionId.toString(),
+        name: res[i].name,
+        image: res[i].artworkUrl100,
+        rss: res[i].url
+      });
+    }
+    return podcastsInfo;
+  });
+}
+
+module.exports = getTopPodcasts;
