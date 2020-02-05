3302de951f2f96568161a69a2464c775c51dca04
Ilya Kuchin <rorz.ivcyb@gmail.com>
2020-02-05 04:22:16 +0300

    Dev (#24)
    * Fix popular podcasts url (#23)

--- src/getTopPodcasts.js
+++ src/getTopPodcasts.js
@@ -1,21 +1,43 @@
 const axios = require('axios');
+const jsdom = require('jsdom');
+const { JSDOM } = jsdom;
 
 const URL =
   'https://rss.itunes.apple.com/api/v1/us/podcasts/top-podcasts/all/10/explicit.json';
 
 function getTopPodcasts() {
-  return axios.get(URL).then(function(response) {
-    const res = response.data.feed.results;
-    let podcastsInfo = [];
-    for (let i = 0; i < res.length; i++) {
-      podcastsInfo.push({
-        id: res[i].id.toString(),
-        name: res[i].name,
-        image: res[i].artworkUrl100,
-        rss: res[i].url
-      });
-    }
-    return podcastsInfo;
+  let podcastsInfo = [];
+  let promises = [];
+  return axios
+    .get(URL)
+    .then(function(response) {
+      const res = response.data.feed.results;
+      for (let i = 0; i < res.length; i++) {
+        podcastsInfo.push({
+          id: res[i].id.toString(),
+          name: res[i].name,
+          image: res[i].artworkUrl100,
+          rss: null
+        });
+        promises.push(getFeedUrl(res[i].url));
+      }
+      return Promise.all(promises);
+    })
+    .then(res => {
+      for (let i = 0; i < res.length; i++) {
+        podcastsInfo[i].rss = res[i];
+      }
+      return podcastsInfo;
+    });
+}
+
+function getFeedUrl(url) {
+  return axios.get(url).then(res => {
+    const dom = new JSDOM(res.data);
+    let t = JSON.parse(
+      dom.window.document.getElementById('shoebox-ember-data-store').text
+    );
+    return t.data.attributes.feedUrl;
   });
 }
 
