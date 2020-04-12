746fd7ee06cc50e7fb2b4cb0debfa13dd920032d
Ilya Kuchin <rorz.ivcyb@gmail.com>
2020-04-12 23:32:54 +0300

    Remove popular podcasts route (#36)

--- src/routes/podcasts.js
+++ src/routes/podcasts.js
@@ -1,27 +1,15 @@
 const express = require('express');
-const {
-  getPodcasts,
-  getPodcast,
-  getTopPodcasts
-} = require('../helpers/podcasts');
+const { getPodcasts, getPodcast } = require('../helpers/podcasts');
 
 const podcasts = express.Router();
-
-podcasts.get('/popular', (req, res) => {
-  getTopPodcasts()
-    .then(topPodcasts => {
-      res.status(200).json([...topPodcasts]);
-    })
-    .catch(() => res.status(500).json({ message: 'Server error' }));
-});
 
 podcasts.get('/', (req, res) => {
   if (req.query.term && req.query.url) {
     res.status(400).json({ message: 'You can pass only one parameter' });
   } else if (req.query.term) {
     getPodcasts(req.query.term)
-      .then(p => res.status(200).json(p))
-      .catch(error => {
+      .then((p) => res.status(200).json(p))
+      .catch((error) => {
         if (error.message === 'Server error') {
           res.status(500).json({ message: 'Server error' });
         } else {
@@ -30,8 +18,8 @@ podcasts.get('/', (req, res) => {
       });
   } else if (req.query.url) {
     getPodcast(req.query.url)
-      .then(podcast => res.status(200).json(podcast))
-      .catch(error => {
+      .then((podcast) => res.status(200).json(podcast))
+      .catch((error) => {
         if (error.message === 'Server error') {
           res.status(500).json({ message: 'Server error' });
         } else {
